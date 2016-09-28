a9e69c72d214e961762813a43490e65896eba14d
Kris <Kris@Kris-PC>
2016-09-28 14:45:22 +0700

    String Utils

---
+++ Progemon/src/manager/FightGameManager.java
@@ -0,0 +1,23 @@
+package manager;
+
+public class FightGameManager {
+	//null
+	
+	
+	/** This method is called before fight starts. */
+	public static void startFight(){
+		
+	}
+	
+	/** This method is called after fight ends. */
+	public static void endFight(){
+		
+	}
+	
+	/** This method is called every turn */
+	public static void runTurn(){
+		
+	}
+	
+
+}
