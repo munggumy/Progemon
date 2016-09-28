12678201c378953174871ec2267d6e05589d3a10
Kris <Kris@Kris-PC>
2016-09-28 13:41:59 +0700

    Savable Stub

---
+++ Progemon/src/utility/SaveUtility.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class SaveUtility {
+	
+	DEFAULT_SAVE_FOLDER = "save_files";
+	DEFAULT_SAVE_FILE = DEFAULT_SAVE_FOLDER + "/default_save.ser";
+	public static final void saveGame(){
+		
+	}
+}
