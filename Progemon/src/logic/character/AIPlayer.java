4ec44fe05a5a7398d4e1356f3c15a0927b3318bf
Kris <Kris@Kris-PC>
2016-09-29 17:50:11 +0700

    Yay

--- Progemon/src/logic/character/AIPlayer.java
+++ Progemon/src/logic/character/AIPlayer.java
@@ -1,12 +1,13 @@
 package logic.character;
 
-import java.util.ArrayList;
-
+import logic.filters.AttackFilter;
+import logic.filters.MoveFilter;
 import logic.terrain.FightMap;
 import logic.terrain.FightTerrain;
 import utility.RandomUtility;
+import utility.Savable;
 
-public class AIPlayer extends Player {
+public class AIPlayer extends Player implements Savable {
 
 	public AIPlayer(String name, Pokemon starter_pokemon) {
 		super(name, starter_pokemon);
@@ -15,18 +16,31 @@ public class AIPlayer extends Player {
 	public AIPlayer(String name) {
 		super(name);
 	}
-	
 
 	@Override
 	public void runTurn(Pokemon pokemon, FightMap fightMap) {
 		int x = pokemon.getX();
 		int y = pokemon.getY();
-		ArrayList<FightTerrain> movableBlocks = fightMap.findMovableBlockAround(pokemon.getMoveRange(),
-				fightMap.getFightTerrainAt(x, y), new ArrayList<FightTerrain>(), pokemon);
-		FightTerrain nextBlock = RandomUtility.randomElement(movableBlocks);
+
+		// move
+		pokemon.findBlocksAround(pokemon.getMoveRange(), new MoveFilter());
+		FightTerrain nextBlock = RandomUtility.randomElement(pokemon.getBlocks());
 		pokemon.move(nextBlock.getX(), nextBlock.getY());
 		System.out.println("Pokemon " + pokemon.getName() + " moved from (" + x + ", " + y + ") to (" + pokemon.getX()
 				+ ", " + pokemon.getY() + ").");
+
+		// attack
+		pokemon.findBlocksAround(pokemon.getAttackRange(), new AttackFilter());
+		for (Pokemon other : fightMap.getPokemonsOnMap()) {
+			
+			if (!other.getOwner().equals(this) && pokemon.getBlocks().contains(other.getCurrentFightTerrain())) {
+				// If other is enemy and in attack range.
+				// pokemon.attack(other, selectedSkill);
+				pokemon.attack(other, 1);
+				
+			}
+			
+		}
 	}
 
 }
