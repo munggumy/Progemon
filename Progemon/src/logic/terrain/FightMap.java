65ce2509dbc6ec1b1f76c1d205c93d98947ac27b
Kris Kalavantavanich <krismath.K@gmail.com>
2016-10-10 03:47:39 +0700

    Implement Pokemon.bySpeed (sort)

--- Progemon/src/logic/terrain/FightMap.java
+++ Progemon/src/logic/terrain/FightMap.java
@@ -54,7 +54,7 @@ public class FightMap implements IRenderable {
 
 	/** Sort by Speed. Used in calculation of Turn Time and Queue. */
 	public void sortPokemons() {
-		Collections.sort(pokemonsOnMap, Pokemon.getSpeedComparator());
+		Collections.sort(pokemonsOnMap, Pokemon.bySpeed);
 	}
 
 	public FightTerrain[][] getMap() {
@@ -100,7 +100,7 @@ public class FightMap implements IRenderable {
 	public void draw() {
 		int x = InputUtility.getMouseX(), y = InputUtility.getMouseY();
 		if (0 <= x && x <= 319 && 0 <= y && y <= 239) {
-			getFightTerrainAt((int) Math.floor(x / 40), (int) Math.floor(y / 40)).setCursur(true);
+			getFightTerrainAt((int) Math.floor(x / 40), (int) Math.floor(y / 40)).setCursor(true);
 		}
 		DrawingUtility.drawFightMap(this);
 	}
