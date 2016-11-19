a513a752be36c82822586b1fc8a5a476b7ef2d80
Kris Kalavantavanich <krismath.K@gmail.com>
2016-11-19 23:00:53 +0700

    Made Frame Deprecated

--- Progemon/src/graphic/Frame.java
+++ Progemon/src/graphic/Frame.java
@@ -10,8 +10,9 @@ import javax.swing.JFrame;
 
 import utility.InputUtility;
 
+@Deprecated
 public class Frame {
-	
+
 	public static final int OFFSET_X = 8, OFFSET_Y = 31;
 
 	private static JFrame frame;
@@ -31,7 +32,7 @@ public class Frame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				super.mouseClicked(e);
-				InputUtility.addInputEvents(e);
+				// InputUtility.addInputEvents(e);
 			}
 
 		});
@@ -40,7 +41,7 @@ public class Frame {
 			@Override
 			public void mouseMoved(MouseEvent e) {
 				super.mouseMoved(e);
-				InputUtility.addInputEvents(e);
+				// InputUtility.addInputEvents(e);
 				InputUtility.setMouseX(e.getX() - OFFSET_X);
 				InputUtility.setMouseY(e.getY() - OFFSET_Y);
 			}
@@ -52,24 +53,24 @@ public class Frame {
 			public void keyPressed(KeyEvent e) {
 				// TODO Auto-generated method stub
 				super.keyPressed(e);
-				InputUtility.addInputEvents(e);
-				InputUtility.addHoldingKeys(e);
-				InputUtility.setLastKeyEvent(e);
+				// InputUtility.addInputEvents(e);
+				// InputUtility.addHoldingKeys(e);
+				// InputUtility.setLastKeyEvent(e);
 			}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 				// TODO Auto-generated method stub
 				super.keyReleased(e);
-				InputUtility.addInputEvents(e);
-				InputUtility.removeHoldingKeys(e);
+				// InputUtility.addInputEvents(e);
+				// InputUtility.removeHoldingKeys(e);
 			}
-			
+
 			@Override
 			public void keyTyped(KeyEvent e) {
 				// TODO Auto-generated method stub
 				super.keyTyped(e);
-				InputUtility.addTypeKeys(e);
+				// InputUtility.addTypeKeys(e);
 			}
 
 		});
