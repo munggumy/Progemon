eda8fdf9e92322270226ca858769012e1409b20a
Kris Kalavantavanich <krismath.K@gmail.com>
2016-11-22 17:23:25 +0700

    Reimplement keyPressed and keyTriggered as Set<KeyCode>

--- Progemon/src/utility/InputUtility.java
+++ Progemon/src/utility/InputUtility.java
@@ -1,19 +1,17 @@
 package utility;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import javafx.scene.input.KeyCode;
 
 public class InputUtility {
 
 	private static int mouseX, mouseY;
-	// private static MouseEvent lastMouseClickEvent, lastMouseMoveEvent;
-	// private static ArrayList<Event> events = new ArrayList<Event>();
 	private static boolean mouseLeftDown, mouseRightDown, mouseOnScreen;
 	private static boolean mouseLeftLastDown, mouseRightLastDown;
-	// private static KeyEvent lastKeyEvent, lastTypeKey;
-	private static ArrayList<KeyCode> keyPressed = new ArrayList<KeyCode>();
-	private static ArrayList<KeyCode> keyTriggered = new ArrayList<KeyCode>();
+	private static Set<KeyCode> keyPressed = new HashSet<KeyCode>();
+	private static Set<KeyCode> keyTriggered = new HashSet<KeyCode>();
 
 	public static int getMouseX() {
 		return mouseX;
@@ -76,10 +74,9 @@ public class InputUtility {
 	}
 
 	public static void setKeyPressed(KeyCode keycode, boolean pressed) {
-		if (!keyPressed.contains(keycode) && pressed) {
+		if (pressed) {
 			keyPressed.add(keycode);
-		}
-		if (keyPressed.contains(keycode) && !pressed) {
+		} else {
 			keyPressed.remove(keycode);
 		}
 	}
@@ -89,97 +86,17 @@ public class InputUtility {
 	}
 
 	public static void setKeyTriggered(KeyCode keycode, boolean pressed) {
-		if (!keyTriggered.contains(keycode) && pressed) {
-			keyTriggered.add(keycode);
-		}
-		if (keyTriggered.contains(keycode) && !pressed) {
-			keyTriggered.remove(keycode);
+		if (pressed) {
+			keyPressed.add(keycode);
+		} else {
+			keyPressed.remove(keycode);
 		}
 	}
 
-	/** Update Trigger Mechanism at end of Clock.tick()*/
+	/** Update Trigger Mechanism at end of Clock.tick() */
 	public static void postUpdate() {
 		setMouseLeftLastDown(false);
 		setMouseRightLastDown(false);
 		keyTriggered.clear();
 	}
-
-	// public static void clear() {
-	// events.clear();
-	// }
-	//
-	// /** When get element, it clears the history of event. */
-	// public static final ArrayList<Event> getEvents() {
-	// ArrayList<Event> temp = new ArrayList<Event>(events);
-	// events.clear();
-	// return temp;
-	// }
-	//
-	// public static final void addEvents(Event event) {
-	// events.add(event);
-	// }
-
-	// public static final MouseEvent getLastMouseClickEvent() {
-	// MouseEvent temp = lastMouseClickEvent;
-	// lastMouseClickEvent = null;
-	// return temp;
-	// }
-	//
-	// public static final void setLastMouseClickEvent(MouseEvent
-	// lastMouseClickEvent) {
-	// InputUtility.lastMouseClickEvent = lastMouseClickEvent;
-	// }
-	//
-	// public static final MouseEvent getLastMouseMoveEvent() {
-	// return lastMouseMoveEvent;
-	// }
-	//
-	// public static final void setLastMouseMoveEvent(MouseEvent
-	// lastMouseMoveEvent) {
-	// InputUtility.lastMouseMoveEvent = lastMouseMoveEvent;
-	// }
-
-	// public static KeyEvent getLastKeyEvent() {
-	// KeyEvent temp = lastKeyEvent;
-	// lastKeyEvent = null;
-	// return temp;
-	// }
-	//
-	// public static void setLastKeyEvent(KeyEvent lastKeyEvent) {
-	// InputUtility.lastKeyEvent = lastKeyEvent;
-	// }
-
-	// public static void addKeys(KeyEvent kEvent) {
-	// boolean isIn = false;
-	// for (KeyEvent holdingKey : keyPressed) {
-	// if (holdingKey.getCode() == kEvent.getCode()) {
-	// isIn = true;
-	// break;
-	// }
-	// }
-	// if (!isIn) {
-	// keyPressed.add(kEvent);
-	// keyTrigger.add(kEvent);
-	// }
-	// }
-	//
-	// public static void removeHoldingKeys(KeyEvent kEvent) {
-	// for (int i = keyPressed.size() - 1; i >= 0; i--) {
-	// if (keyPressed.get(i).getCode() == kEvent.getCode()) {
-	// keyPressed.remove(i);
-	// break;
-	// }
-	// }
-	// }
-	//
-	// public static ArrayList<KeyEvent> getHoldingKeys() {
-	// return keyPressed;
-	// }
-	//
-	// public static ArrayList<KeyEvent> getTypeKeys() {
-	// ArrayList<KeyEvent> token = new ArrayList<KeyEvent>(keyTrigger);
-	// keyTrigger.clear();
-	// return token;
-	// }
-
 }
